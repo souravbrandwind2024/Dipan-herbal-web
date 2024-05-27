90b512ccd4f71205954700c40656d71c31c184bc
souravbrandwind2024 <sourav@brandwind.in>
2024-05-27 18:11:52 +0530

    product list page left side section design done

--- js/main.js
+++ js/main.js
@@ -189,6 +189,52 @@ $(".weight-label.area1 .waitbtn li").click(function(){
 // product-details-size-clickable-area-end //
    
 
+// product-list-range-slider-area-start //
+(function() {
+  var parent = document.querySelector(".range-slider");
+  if(!parent) return;
+
+  var
+    rangeS = parent.querySelectorAll("input[type=range]"),
+    numberS = parent.querySelectorAll("input[type=number]");
+
+  rangeS.forEach(function(el) {
+    el.oninput = function() {
+      var slide1 = parseFloat(rangeS[0].value),
+            slide2 = parseFloat(rangeS[1].value);
+
+      if (slide1 > slide2) {
+                [slide1, slide2] = [slide2, slide1];
+        // var tmp = slide2;
+        // slide2 = slide1;
+        // slide1 = tmp;
+      }
+
+      numberS[0].value = slide1;
+      numberS[1].value = slide2;
+    }
+  });
+
+  numberS.forEach(function(el) {
+    el.oninput = function() {
+            var number1 = parseFloat(numberS[0].value),
+                    number2 = parseFloat(numberS[1].value);
+            
+      if (number1 > number2) {
+        var tmp = number1;
+        numberS[0].value = number2;
+        numberS[1].value = tmp;
+      }
+
+      rangeS[0].value = number1;
+      rangeS[1].value = number2;
+
+    }
+  });
+})();
+// product-list-range-slider-area-end //
+
+
 
 });
 
@@ -236,6 +282,11 @@ $(".weight-label.area1 .waitbtn li").click(function(){
 })();
 
 inputNumber($('.updown-btn .input-number'))
+
+
+
+
+
 
 
 
